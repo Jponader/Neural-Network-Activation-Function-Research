a128a2321e56d21bbb360f05329d1188a075be9d
Jponader <jponader@knights.ucf.edu>
2020-03-28 11:16:36 -0400

    updated testing data storage

--- tester.py
+++ tester.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from tensorflow import keras
 from datetime import datetime
-from tensorflow.keras.activations import relu, sigmoid, tanh, linear, elu, exponential, hard_sigmoid, softplus, softsign
+from tensorflow.keras.activations import relu, sigmoid, tanh, linear, elu, exponential
 import h5py
 import time
 import sys
@@ -9,19 +9,26 @@ import os
 
 from networks import *
 
-assert tf.test.is_built_with_cuda(), "Cudo Not Working"
-assert tf.test.is_gpu_available(cuda_only=False, min_cuda_compute_capability=None), "Not Using GPU"
+if os.path.exists("Results.txt"):
+	os.remove("Results.txt")
+
+f = open("Results.txt", "a")
+
+#assert tf.test.is_built_with_cuda(), "Cudo Not Working"
+#assert tf.test.is_gpu_available(cuda_only=False, min_cuda_compute_capability=None), "Not Using GPU"
 # SET PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.1\extras\CUPTI\lib64;%PATH%
 
 
 networks = [FashionMNIST, CNN_MNIST, CIFAR]
 
 #Speed Test
-#networks = [CIFAR]
+#networks = [FashionMNIST]
 
 activationfunctions = [relu, sigmoid, tanh, linear, elu, exponential]
 
-for runs in range(5):
+#activationfunctions = [relu]
+
+for runs in range(2):
 	for network in networks:
 		net = network()
 		config = net.getConfig()
@@ -34,9 +41,20 @@ for runs in range(5):
 			log_dir = os.path.join(config['path'],"logs",func.__name__ , str(runs))
 			tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=0)
 
-			model.fit(xTrain, yTrain, epochs= config['epoch'], batch_size = config['batch'],validation_data =(xTest, yTest), callbacks = [tensorboard_callback], use_multiprocessing=True)
+			history = model.fit(xTrain, yTrain, epochs= config['epoch'], batch_size = config['batch'],validation_data =(xTest, yTest), callbacks = [tensorboard_callback], use_multiprocessing=True)
+
+			for keys in history.history.keys():
+				f.write(str(network.__name__) +','+ str(func.__name__) +','+ str(runs) + ',' + keys)
+				for val in history.history[keys]:
+					f.write(','+str(val))
+				f.write("\n")
 
 			#Speed Train for Testin
 			#model.fit(xTrain, yTrain, epochs= 5, batch_size = config['batch'],validation_data =(xTest, yTest), callbacks = [cp_callback, tensorboard_callback])
 
 			del model
+
+f.close()
+
+
+
